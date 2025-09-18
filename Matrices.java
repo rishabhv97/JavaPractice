c665ea3222de65b3cfc1ac3f6c2d2b3a5953e0ea
Rishabh <rishabhv2100@gmail.com>
2025-09-18 20:41:09 +0530

    Matrices code updated

--- Matrices.java
+++ Matrices.java
@@ -89,6 +89,29 @@ public class Matrices {
         System.out.println("Key Not found");
         return false;
     }
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i=0;i< matrix.length-1; i++){
+            for(int j=0; j<matrix[i].length-1; j++){
+                if(matrix[i][j]!=matrix[i+1][j+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public int[] twoSum(int[] nums, int target) {
+        int arr[]=new int[2];
+        for(int i=0; i<nums.length ; i++){
+            for(int j=1; j<nums.length ; j++){
+                if (nums[i]+nums[j]==target) {
+                    arr[0]=i;
+                    arr[1]=j;
+                }
+            }
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         // int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
         // printSpiral(matrix);
