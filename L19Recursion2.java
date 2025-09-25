617291fbed3a76d80fbb3bb6dd0fdd11c75effe7
Rishabh <rishabhv2100@gmail.com>
2025-09-25 20:15:14 +0530

    update recursion code

--- L19Recursion2.java
+++ L19Recursion2.java
@@ -29,9 +29,28 @@ public class L19Recursion2 {
             removeDuplicates(str, idx+1, newStr.append(currChar), map);
         }
     }
+
+    public static void printBinStrings(int n, int lastPlace, String str){
+        if (n==0) {
+            System.out.println(str);
+            return;
+        }
+
+        printBinStrings(n-1,0, str+"0");
+        if (lastPlace==0) {
+            printBinStrings(n-1, 1, str+"1");
+        }
+        // if (lastPlace==0) {
+        //     printBinStrings(n-1, 0, str.append("0"));
+        //     printBinStrings(n-1, 1, str.append("1"));
+        // }else{
+        //     printBinStrings(n-1,0, str.append("0"));
+        // }
+    }
     public static void main(String[] args) {
         // System.out.println(tileProblem(6));
-        String str ="appnnacollege";
-        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+        // String str ="appnnacollege";
+        // removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+        printBinStrings(3,0,"");
     }
 }
